9a1bea57b03aa5586a91d69af9fa00e81e27c55e
sabiaparveen1 <sabia.parveen@nashtechglobal.com>
2024-01-18 17:06:18 +0530

    resolved code smells from web and common

--- nashtech-web/src/test/java/com/nastechglobal/web/WebClientServiceTest.java
+++ nashtech-web/src/test/java/com/nastechglobal/web/WebClientServiceTest.java
@@ -32,23 +32,40 @@ class WebClientServiceTest {
     private static final String BODY = "Test content";
 
     private HttpEntity httpEntity;
+    private HttpEntity httpEntity1;
 
     @BeforeEach
     void init() {
         MockitoAnnotations.openMocks(this);
         httpEntity = new HttpEntity<>(BODY, HttpHeaders.EMPTY);
+        httpEntity1=new HttpEntity<>(null, HttpHeaders.EMPTY);
     }
 
     @Test
     void test() {
-        ExternalApiRequest<Object> testContent = ExternalApiRequest.builder()
+        ExternalApiRequest<Object> testContentWithBody = ExternalApiRequest.builder()
                 .url(URL)
                 .httpMethod(HttpMethod.GET)
                 .body(BODY)
                 .headers(HttpHeaders.EMPTY)
                 .build();
-        when(restTemplate.exchange(URL, HttpMethod.GET, httpEntity, String.class)).thenReturn(ResponseEntity.ok(BODY));
-        ResponseEntity<String> externalApiResponse = webClientService.getExternalApiResponse(testContent, String.class);
-        assertEquals(HttpStatusCode.valueOf(200), externalApiResponse.getStatusCode());
+
+        ExternalApiRequest<Object> testContentWithoutBody = ExternalApiRequest.builder()
+                .url(URL)
+                .httpMethod(HttpMethod.GET)
+                .headers(HttpHeaders.EMPTY)
+                .build();
+
+        // Test case with body
+        when(restTemplate.exchange(URL, HttpMethod.GET, httpEntity, String.class))
+                .thenReturn(ResponseEntity.ok(BODY));
+        ResponseEntity<String> externalApiResponseWithBody = webClientService.getExternalApiResponse(testContentWithBody, String.class);
+        assertEquals(HttpStatusCode.valueOf(200), externalApiResponseWithBody.getStatusCode());
+
+        // Test case without body
+        when(restTemplate.exchange(URL, HttpMethod.GET, httpEntity1, String.class))
+                .thenReturn(ResponseEntity.ok(BODY));
+        ResponseEntity<String> externalApiResponseWithoutBody = webClientService.getExternalApiResponse(testContentWithoutBody, String.class);
+        assertEquals(HttpStatusCode.valueOf(200), externalApiResponseWithoutBody.getStatusCode());
     }
 }
